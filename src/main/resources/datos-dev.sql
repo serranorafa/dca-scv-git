b22303b4f78d89518d82f25c5de49ddff362d715
Rafael Serrano Aracil <rsa26@alu.ua.es>
2020-12-27 14:50:36 +0100

    Añadido un equipo nuevo a la BDD

--- src/main/resources/datos-dev.sql
+++ src/main/resources/datos-dev.sql
@@ -13,3 +13,4 @@ INSERT INTO equipo_usuario (fk_equipo, fk_usuario) VALUES('1', '1');
 INSERT INTO equipo_usuario (fk_equipo, fk_usuario) VALUES('1', '2');
 INSERT INTO equipos (id, nombre) VALUES('2', 'Proyecto P0');
 INSERT INTO usuarios (id, email, nombre, password, fecha_nacimiento, admin, bloqueado) VALUES('4', 'unusuario@ua', 'Señor Usuario', '123', '1992-12-25', false, false);
+INSERT INTO equipos (id, nombre) VALUES('3', 'Equipo número 5');
